9f69e027cd553e48269fb951de78b68cfcc40e54
montero-melis <montero-melis@biling.su.se>
2016-07-04 00:10:44 +0200

    Finish analyses 1+2 and plots

--- analysis.R
+++ analysis.R
@@ -10,6 +10,13 @@ library(ggplot2)
 et <- read.csv("data_triads.csv", encoding = "UTF-8")
 
 
+
+# Global plotting parameters ----------------------------------------------
+
+ggtheme <- theme(text = element_text(size = 12))
+mydpi <- 450
+
+
 # Prepare data for model fitting ------------------------------------------
 
 contrasts(et$LanguageType) <- matrix(
@@ -54,9 +61,9 @@ summary(fm_an1_nested)
 
 
 
-# Plot analysis 1 ---------------------------------------------------------
+# Plot results analysis 1 -------------------------------------------------
 
-# We will plot by-participant averages and language confidence intervals
+# We will plot by-participant averages and by-language confidence intervals
 et_bs <- et %>%
   group_by(Participant, language, LanguageType) %>%
   summarise(SameManner = mean(SameMannerResponse))
@@ -71,30 +78,32 @@ et_bs$language <- factor(et_bs$language, levels = ordered_langs)
 rm(manner_language, ordered_langs)
 
 # plot with jitter
-langplot <- ggplot(
+plot_main <- ggplot(
   et_bs, aes(x = language, y = SameManner, color = LanguageType,
              shape = LanguageType, linetype = LanguageType)) +
-  geom_jitter(alpha = .4, size = 2, position = position_jitter(width = .75,
+  geom_jitter(alpha = .5, size = 1.5, position = position_jitter(width = .75,
                                                                height = 0)) +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size = 1, width = .8) +
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size = .75, width = .75) +
+  xlab("Language") +
+  ylab("Proportion of same-manner responses") +
+  theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         axis.title.y=element_text(vjust = 1), legend.position = "top") 
-langplot
-# one might want to add by-type means
+plot_main
+# compute by-type means
 type_mean <- et %>%
   group_by(LanguageType) %>%
   summarise(M = mean(SameMannerResponse))
-langplot <- langplot + geom_hline(
-  data = type_mean, aes(yintercept = M, colour = LanguageType,
-                        linetype = LanguageType), size = 1)
-langplot
-# and suitable axis labels
-langplot <- langplot + 
-  xlab("Language") +
-  ylab("Proportion of same-manner responses")
-langplot
+# add them to plot
+plot_main <- plot_main + 
+  geom_hline(data = type_mean, aes(yintercept = M, colour = LanguageType,
+                                   linetype = LanguageType), size = 1)
+set.seed(564)  # make plot reproducible
+plot_main + ggtheme
 
-ggsave("figures/manner-by-language.png", width = 7, height = 5)
+# save to disk
+ggsave("figures/manner-by-language.pdf", width = 7, height = 5)
+ggsave("figures/manner-by-language.tiff", width = 7, height = 5, dpi = mydpi)
 
 
 
@@ -127,273 +136,35 @@ summary(fm_an2_trial1)
 
 
 
+# Plot results analysis 2 -------------------------------------------------
 
 
-
-
-
-
-
-
-### Recycle code below as needed, delete the rest
-
-
-################## beginning GMM edits
-
-## Let's plot the effect of response to the first trial
-# on the remainder of the experiment.
-library(ggplot2)
-
-theme_set(theme_bw(base_size=14))
-
-# simplify the data frame
-trial1 <- d.sub[, c("language", "SameMannerResponse", "LanguageType", "Participant",
-                    "TrialOrder", "LanguageCommunity", "FirstTrial.SameMannerResponse")]
 # better and shorter variable name for plotting
-trial1$FirstTrial <- ifelse(trial1$FirstTrial.SameMannerResponse == 1, "same-manner", "same-path")
-trial1$FirstTrial.SameMannerResponse <- NULL
-# now remove all first trial observations from data (TrialOrder == 5)
-trial1 <- trial1[trial1$TrialOrder != 5, ]
-# compute by-subject averages, keeping info about language, language type, etc
-tr1_bs <- ddply(trial1, .(language, LanguageType, Participant, LanguageCommunity,
-                          FirstTrial), summarise,
-                MannerChoice = mean(SameMannerResponse))
+et_tr1$FirstTrial <- ifelse(et_tr1$FirstTrial.SameMannerResponse == 1,
+                            "Same-manner", "Same-path")
 
-# first check if Language Type seems to play a role here, i.e. whether it
-# interacts with effect of choice on first trial
-ggplot(tr1_bs, aes(x = FirstTrial, y = MannerChoice, colour = LanguageType)) +
-  geom_boxplot()
-# really doesn't seem like it does, so let's just plot individual points and C.I.
-ggplot(tr1_bs, aes(x = FirstTrial, y = MannerChoice)) +
-  geom_jitter(size = 2, alpha = .4, position = position_jitter(width = .2, height = .01)) +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size = 1.5, width = .5) +
-  xlab("choice on first trial") + ylab("proportion\nof same-manner responses") +
-  theme(axis.title.y=element_text(vjust = 1)) 
+# compute by-subject averages
+et_tr1_bs <- et_tr1 %>%
+  group_by(LanguageType, Participant, FirstTrial) %>%
+  summarise(MannerChoice = mean(SameMannerResponse))
+
+# There is one row with missing values, remove it
+sum(!complete.cases(et_tr1_bs))
+et_tr1_bs <- et_tr1_bs[complete.cases(et_tr1_bs), ]
+
+# Plot participants as points and 95% bootstrapped C.I.
+plot_trial1 <- ggplot(et_tr1_bs, aes(x = FirstTrial, y = MannerChoice)) +
+  geom_jitter(position = position_jitter(width = .2, height = .025),
+              size = 1, alpha = .3, ) +
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size = 1,
+               width = .5) +
+  xlab("Choice on first trial") + ylab("Proportion of\nsame-manner responses") +
+  theme_bw() +
+  theme(axis.title.y=element_text(vjust = 1)) +
+  ggtheme
+set.seed(297)  # make plot reproducible
+plot_trial1
 
 # save to disk
-ggsave("figures/first-trial-effect.png", width = 3.25, height = 3.5)
-
-################## end GMM edits
-
-
-
-# a simplified model to investigate the variance by participants within verb- and 
-# satellite-framed languages.
-g4 = glmer(SameMannerResponse ~ 
-            LanguageType +
-            (1 | Participant),
-          d.sub, family = "binomial"
-)
-summary(g4)
-
-g4a = glmer(SameMannerResponse ~ 
-             1 +
-             (1 | Participant),
-           d.sub, family = "binomial", subset = LanguageType == "verb-framed"
-)
-g4b = glmer(SameMannerResponse ~ 
-             1 +
-             (1 | Participant),
-           d.sub, family = "binomial", subset = LanguageType != "verb-framed"
-)
-summary(g4a)
-summary(g4b)
-
-library(plyr)
-library(ggplot2)
-
-contrasts(d.sub$LanguageType) = matrix(c(1,-1), nrow=2, ncol=1, byrow=F, 
-                                       dimnames= list(c("sattelite-framed", "verb-framed"), c("SatteliteVsVerb"))
-)
-
-dd = ddply(d.sub, 
-           .(Participant, language, LanguageType, LanguageCommunity),
-           summarise,
-           SameMannerResponse = mean(SameMannerResponse))
-# (GMM) at least for plotting we want the language names to be transparent
-dd$language <-
-  revalue(dd$language, 
-          c("ba" = "Basque", "ca" = "Catalan", "du" = "Dutch", "en" = "English",
-            "es" = "Estonian", "Fr" = "French", "ge" = "German", "hi" = "Hindi",
-            "it" = "Italian", "ja" = "Japanese", "jo" = "Jalonke", "la" = "Lao",
-            "po" = "Polish", "ru" = "Russian", "sp" = "Spanish", "ta" = "Tamil",
-            "ti" = "Tidore", "tu" = "Turkish", "ty" = "Tiriyo", "yu" = "Yukatek"))
-theme_set(theme_bw(base_size=18))
-ggplot(dd, aes(x = language, y = SameMannerResponse, color = LanguageType)) +
-  geom_hline(yintercept = mean(dd$SameMannerResponse), color = "gray") +
-  geom_point(alpha = .4, size = 3) +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size = 1, width = .3) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-################## GMM edits starts
-# (GMM) order languages by average SameMannerResponse
-lang_ord <- ddply(dd, .(language), summarise, M = mean(SameMannerResponse))
-myorder <- lang_ord[order(lang_ord$M), "language"]
-dd$language <- factor(dd$language, levels = myorder)
-rm(lang_ord, myorder)
-
-theme_set(theme_bw(base_size=14))
-# plot with jitter
-langplot <- ggplot(dd, aes(x = language, y = SameMannerResponse, 
-                           color = LanguageType, shape = LanguageType,
-                           linetype = LanguageType)) +
-  geom_jitter(alpha = .4, size = 2, 
-              position = position_jitter(width = .75, height = 0)) +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size = 1, width = .8) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.y=element_text(vjust = 1), legend.position = "top") 
-#   legend.justification=c(0,1), legend.position=c(0,1),
-#         legend.background = element_rect(fill="gray95"))
-langplot
-# one might want to add by-type means
-type_mean <- ddply(d.sub, .(LanguageType), summarise, M = mean(SameMannerResponse))
-langplot <- langplot + geom_hline(data = type_mean, 
-                                  aes(yintercept = M, colour = LanguageType,
-                                      linetype = LanguageType), size = 1)
-langplot
-# and suitable y-axis label
-langplot <- langplot + ylab("Proportion of same-manner responses")
-langplot
-
-ggsave("figures/manner-by-language.png", width = 7, height = 5)
-
-
-
-## Language community: small/non-standardized vs. large/standardized
-# see also analyses below towards end of script
-
-# plot with jitter
-langsizeplot <- ggplot(dd, aes(x = language, y = SameMannerResponse,
-                               color = LanguageCommunity, shape = LanguageCommunity,
-                               linetype = LanguageCommunity)) +
-  geom_jitter(alpha = .4, size = 2, 
-              position = position_jitter(width = .7, height = .01)) +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", size = 1, width = .3) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title.y=element_text(vjust = 1), legend.position = "top") +
-#         legend.justification=c(0,1), legend.position=c(0,1),
-#         legend.background = element_rect(fill="gray95")) +
-  scale_color_brewer(palette="Dark2")
-langsizeplot
-# one might want to add by-type means
-commun_mean <- ddply(d.sub, .(LanguageCommunity), summarise, M = mean(SameMannerResponse))
-langsizeplot <- langsizeplot + geom_hline(data = commun_mean, 
-                                  aes(yintercept = M, colour = LanguageCommunity),
-                                  linetype = "dashed", size = 1)
-langsizeplot
-# and suitable y-axis label
-langsizeplot <- langsizeplot + ylab("Proportion of same-manner responses")
-langsizeplot
-
-ggsave("figures/manner-by-language_size.png", width = 6.5, height = 5)
-
-################## end GMM edits
-
-# simple random effect structure plus covariates
-contrasts(d.sub$tar_path) = contr.sum(2)
-contrasts(d.sub$tar_man) = contr.sum(4)
-contrasts(d.sub$path_ch) = contr.sum(2)
-contrasts(d.sub$man_ch) = contr.sum(2)
-
-g1a = glmer(SameMannerResponse ~ 
-	LanguageType +
-	tar_path *
-	tar_man +
-	(1 | ItemScene) +
-	(1 | ItemWithinScene),
-	d.sub, family = "binomial"
-)
-
-g2a = glmer(SameMannerResponse ~ 
-	LanguageType +
-	tar_path *
-	tar_man +
-	(1 | language) +
-	(1 | ItemScene) +
-	(1 | ItemWithinScene),
-	d.sub, family = "binomial"
-)
-
-g2a2 = glmer(SameMannerResponse ~ 
-              LanguageType +
-              tar_path *
-              tar_man * 
-              man_ch +
-              (1 | language) +
-              (1 | ItemScene) +
-              (1 | ItemWithinScene),
-            d.sub, family = "binomial"
-)
-summary(g1a)
-summary(g2a)
-summary(g2a2)
-## ------------------------------------------------------------------------------
-#
-# Community size / Standardization
-#
-# Most lgs in our sample come from large communities with standardized language. 
-# Standardization might make participants' responses less variable. At the same time
-# larger lg communities (where individuals are not in constant contact with each other)
-# might have more variability between participants, compared to data from participants
-# from small lg communities (especially if they were collected under the typical field
-# work setting, all from one village).
-# 
-## ------------------------------------------------------------------------------
-
-g5 = glmer(SameMannerResponse ~ 
-             LanguageCommunity +
-             (1 | Participant),
-           d.sub, family = "binomial"
-)
-summary(g5)
-
-# Add same random effects structure as above
-g5a = glmer(SameMannerResponse ~ 
-              scale(as.numeric(LanguageCommunity), scale = F) +
-              (1 | language) +
-              (1 | Participant) +
-              (1 | ItemScene) +
-              (1 | ItemWithinScene),
-            d.sub, family = "binomial"
-)
-summary(g5a)
-
-# add LanguageType and interaction as fixed effects
-g5b = glmer(SameMannerResponse ~ 
-              scale(as.numeric(LanguageCommunity), scale = F) *
-              LanguageType +
-              (1 | language) +
-              (1 | Participant) +
-              (1 | ItemScene) +
-              (1 | ItemWithinScene),
-            d.sub, family = "binomial"
-)
-summary(g5b)
-
-
-g5_large_standard = glmer(SameMannerResponse ~ 
-              1 +
-              (1 | Participant),
-            d.sub, family = "binomial", subset = LanguageCommunity == "large/standardized"
-)
-g5_small_nonstandard = glmer(SameMannerResponse ~ 
-              1 +
-              (1 | Participant),
-            d.sub, family = "binomial", subset = LanguageCommunity != "large/standardized"
-)
-summary(g5_large_standard)
-summary(g5_small_nonstandard)
-
-# effects of language type? 
-g6_large_standard = glmer(SameMannerResponse ~ 
-                            LanguageType +
-                            (1 | Participant),
-                          d.sub, family = "binomial", subset = LanguageCommunity == "large/standardized"
-)
-g6_small_nonstandard = glmer(SameMannerResponse ~ 
-                               LanguageType +
-                               (1 | Participant),
-                             d.sub, family = "binomial", subset = LanguageCommunity != "large/standardized"
-)
-summary(g6_large_standard)
-summary(g6_small_nonstandard)
+ggsave("figures/first-trial-effect.pdf", width = 3, height = 3.5)
+ggsave("figures/first-trial-effect.tiff", width = 3, height = 3.5, dpi = mydpi)
